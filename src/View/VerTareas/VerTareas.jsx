b5fd43c15724151dc9e70cd186a6623a1fcf91cb
GastonYasuda <gastonyasuda@gmail.com>
2023-07-01 01:11:22 -0300

    change apiContext

--- src/View/VerTareas/VerTareas.jsx
+++ src/View/VerTareas/VerTareas.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { ApiContext } from '../../Context/ApiProvider'
 import { Link } from 'react-router-dom';
+import { ApiContext } from '../../Context/apiProvider';
 
 
 
