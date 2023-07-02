5a235ff5d4c60227c03e5628ce8b170df26edec2
GastonYasuda <gastonyasuda@gmail.com>
2023-07-02 18:03:43 -0300

    apiProvider error

--- src/View/VerTareas/VerTareas.jsx
+++ src/View/VerTareas/VerTareas.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
-import { ApiContext } from '../../Context/apiProvider';
+import { ApiContext } from '../../Context/ApiProvider';
 
 
 
