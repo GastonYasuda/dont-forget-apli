7489c1830a1ab2392123c858f9369fbc609ea5dd
GastonYasuda <gastonyasuda@gmail.com>
2023-08-15 22:53:53 -0300

    home style

--- src/View/Home/Home.jsx
+++ src/View/Home/Home.jsx
@@ -34,26 +34,50 @@ const Home = () => {
                 <div onClick={isLoged} className='homeStyle__task-card b-radius-15'>
                     {
                         guestItem !== null ?
-                            <Link to={`/asignar-tarea`}>
-                                <span>ASSIGN TASK</span>
-                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi, consequuntur reprehenderit asperiores esse blanditiis rerum architecto maxime quas error distinctio, aut facere eaque impedit voluptate nisi rem adipisci, nobis cum.</p>
+                            <Link to={`/asignar-tarea`} className='d-f-col card-justify'>
+                                <div>
+                                    <span>ASSIGN TASK</span>
+                                </div>
+                                <div>
+                                    <img src="./assets/tasks-bg.png" className='imagen' alt="tasks background img" />
+                                </div>
+                                <div>
+                                    <p>Here you can create a list of tasks where you can add it with date.
+                                        You can check or delete these tasks and they are saved for when you enter again.
+                                        <br />You must be logged in to use this option.</p>
+                                </div>
                             </Link>
                             :
                             <Link to={`/`}>
                                 <span>ASSIGN TASK</span>
                             </Link>
                     }
+                    <div className='card-bg '>
+                        <img src="./assets/tasks-bg.png" className='card-bg-img' alt="tasks img" />
+                    </div>
                 </div>
                 <ToastContainer className="mar-top-25" />
             </div>
 
 
             <div className='homeStyle__task'>
-                <div className='homeStyle__task-card quickColor b-radius-15'>
-                    <Link to={`/tarea-rapida`}>
-                        <span>QUICK TASKS</span>
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi, consequuntur reprehenderit asperiores esse blanditiis rerum architecto maxime quas error distinctio, aut facere eaque impedit voluptate nisi rem adipisci, nobis cum.</p>
+                <div className='homeStyle__task-card b-radius-15'>
+                    <Link to={`/tarea-rapida`} className='d-f-col card-justify'>
+                        <div>
+                            <span>QUICK TASKS</span>
+                        </div>
+                        <div>
+                            <img src="./assets/quick_tasks-bg.png" className='imagen' alt="quick tasks img" />
+                        </div>
+                        <div>
+                            <p>Here you can create tasks quickly to have a list to use at that time.</p>
+                        </div>
+
                     </Link>
+
+                    <div className='card-bg '>
+                        <img src="./assets/quick_tasks-bg.png" className='card-bg-img' alt="quick tasks background img" />
+                    </div>
 
                 </div>
             </div>
