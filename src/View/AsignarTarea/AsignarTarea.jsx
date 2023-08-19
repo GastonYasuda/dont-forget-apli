23aa54fa6b05d0b2838d91fde57a35ae66322660
GastonYasuda <gastonyasuda@gmail.com>
2023-08-18 21:48:14 -0300

    .

--- src/View/AsignarTarea/AsignarTarea.jsx
+++ src/View/AsignarTarea/AsignarTarea.jsx
@@ -1,13 +1,13 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { Link } from 'react-router-dom';
+import React, { useContext, useState } from 'react'
 import { ApiContext } from '../../Context/ApiContext';
 import TaskListContainer from '../../Components/TaskListContainer/TaskListContainer';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
-import moment from 'moment';
-import 'moment/locale/es-mx';
 import Button from 'react-bootstrap/esm/Button';
 import Navbar from '../Navbar/Navbar';
+import './asignarTarea.css'
+import moment from 'moment';
+import 'moment/locale/es-mx';
 moment.locale('es-mx')
 
 
@@ -32,28 +32,36 @@ const AsignarTarea = () => {
         <div className='loginBody'>
             <Navbar />
 
-            <h4>AsignarTarea</h4>
-            <form onSubmit={handleSubmit}>
+            <section className='assignForm-body'>
 
-                <FloatingLabel controlId="floatingInput" label="Date">
-                    <Form.Control type="date" placeholder="Date" onChange={e => setInputDate(e.target.value)} value={inputDate} />
-                </FloatingLabel>
+                <div className='assignForm b-radius-15'>
+                    <div className='assignForm__container'>
+                        <h4 className='title'>Assign task</h4>
+                        <form onSubmit={handleSubmit} className='assignForm__container-items d-f-row'>
 
-                <FloatingLabel
-                    controlId="floatingInput"
-                    label="Task"
-                    className="mb-3"
-                >
-                    <Form.Control type="text" placeholder="Task" onChange={e => setInputValue(e.target.value)} value={inputValue} />
-                </FloatingLabel>
+                            <FloatingLabel controlId="floatingInput" label="Date">
+                                <Form.Control type="date" placeholder="Date"  onChange={e => setInputDate(e.target.value)} value={inputDate} />
+                            </FloatingLabel>
 
+                            <FloatingLabel
+                                controlId="floatingInput"
+                                label="Task"
+                                className=" "
+                            >
+                                <Form.Control type="text" placeholder="Task" onChange={e => setInputValue(e.target.value)} value={inputValue} />
+                            </FloatingLabel>
 
+                            <Button type='submit'>ok</Button>
 
-                <Button type='submit'>ok</Button>
+                        </form>
+                    </div>
 
-            </form>
+                </div>
 
-            <TaskListContainer />
+                <TaskListContainer />
+
+            </section>
+
 
         </div>
     )
